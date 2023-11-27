6393a224ee8bb10df71740a54326d19a1a8996df
RuedigerDieter <sharafemil@gmail.com>
2023-11-27 21:30:42 +0100

    adjusted output files

--- Blatt06/job_script.sh
+++ Blatt06/job_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1                      # Anzahl der Knoten
 #SBATCH -n 5                     # Anzahl der Prozesse (5 Knoten * 5 Prozesse)
-#SBATCH -o partdiff.out
+#SBATCH -o timempi.out
 #SBATCH -p west
 srun -N 1 -n 5 -p west timempi.sh
 
-echo "fertig" >> timescript.out
+echo "fertig" >> job.out
