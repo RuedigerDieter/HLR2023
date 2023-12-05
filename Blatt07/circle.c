e42502ed578b7b52a3b8767e165ebedd8be96409
Emil Sharaf <sharafemil@gmail.com>
2023-12-05 13:22:00 +0100

    rough groundwork for sending, recv

--- Blatt07/circle.c
+++ Blatt07/circle.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
+
 
 int*
 init (int N)
@@ -20,9 +22,26 @@ init (int N)
 }
 
 int
-circle (int* buf)
+circle (int* buf, int array_size, int rank)
 {
-	// TODO
+	int* tmp;
+
+	tmp = (int*)malloc(sizeof(int) * array_size);
+
+	tmp = buf;
+	
+	if (rank % 2)
+	{
+		MPI_Ssend(buf, array_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Recv(buf, array_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	}
+	else
+	{
+		MPI_Recv(buf, array_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Ssend(tmp, array_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	
+
 	return 0;
 }
 
@@ -32,7 +51,10 @@ main (int argc, char** argv)
 	int N;
 	int rank;
 	int* buf;
-  int ret;
+  	int ret;
+
+	int world_size;
+	int proc_array_size;
 
 	if (argc < 2)
 	{
@@ -42,26 +64,59 @@ main (int argc, char** argv)
 
 	// Array length
 	N = atoi(argv[1]);
-	buf = init(N);
 
-	// TODO
-	rank = 0;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	printf("\nBEFORE\n");
+	proc_array_size = N / world_size;
+	proc_array_size += (rank < N % world_size) ? 1 : 0;
+	// TODO: Prozesse müssen wissen, wie groß ihr Array ist, bzw. Reste verteilen
 
-	for (int i = 0; i < N; i++)
+	buf = init(proc_array_size);
+
+	// TODO: zwei verschiedene verhalten, je nachdem ob man kleine oder Restgröße hat?
+
+	if (rank == 0)
 	{
-		printf("rank %d: %d\n", rank, buf[i]);
-	}
+		printf("\nBEFORE\n");
+		// TODO: print whole array
+		for (int i = 0; i < proc_array_size; i++)
+		{
+			printf("rank %d: %d\n", i, buf[i]);
+		}
+		printf("rank 0: %d\n", buf[]);
+		for (int i = 1; i < world_size; i++)
+		{
+			// TODO: print whole array
+			MPI_Recv(buf, proc_array_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			for
+			printf("rank %d: %d\n", i, buf[i]);
+		}
 
+		
+	}
+	else
+	{
+		MPI_Send(buf, proc_array_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
 	ret = circle(buf);
-
-	printf("\nAFTER\n");
-
-	for (int j = 0; j < N; j++)
+	if (rank == 0)
 	{
-		printf("rank %d: %d\n", rank, buf[j]);
+		printf("\nAFTER\n");
+
+		for (int i = 1; i < world_size; i++)
+		{
+			MPI_Recv(buf, proc_array_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			printf("rank %d: %d\n", i, buf[j]);
+		}
 	}
+	else
+	{
+		MPI_Ssend(buf, proc_array_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	free(buf);
+	MPI_Finalize();
 
 	return EXIT_SUCCESS;
 }
