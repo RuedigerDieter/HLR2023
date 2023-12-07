c2f84dad5dd2c076b38da95cc5fc6c198bb0d603
RuedigerDieter <sharafemil@gmail.com>
2023-12-07 14:58:23 +0100

    final commit :))

--- Blatt07/circle.c
+++ Blatt07/circle.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include <mpi.h>
+#include <string.h>
 
 
 int*
 init (int N, int rank)
 {
-	// TODO
 	int* buf = (int*)malloc(sizeof(int) * N);
-
-	srand(time(NULL) + rank * 100);
+	/*
+	 * Rang und Faktor um die Streuung der Zufallszahlen zu erhöhen
+	 */
+	srand(time(NULL) + rank * 3892);
 
 	for (int i = 0; i < N; i++)
 	{
@@ -24,57 +27,76 @@ init (int N, int rank)
 int
 circle (int* buf, int array_size, int world_size, int rank)
 {
-	int* tmp;
+	int* tmp_in;
+	int* tmp_out;
 	int* firstbuf;
 	int b_iterate = 0;
+	int iterations = 0;
 
-	int src = (rank == 0)? world_size : rank - 1;
-	int dest = (rank == world_size)? 0 : rank + 1;
+	int src = (rank == 0)? world_size - 1 : rank - 1;
+	int dest = (rank == world_size - 1)? 0 : rank + 1;
 
-	tmp = malloc(sizeof(int) * array_size);
-	firstbuf = (rank == 0)?  NULL : malloc(sizeof(int) * array_size);
+	/**
+	 * Pufferarrays zur Kommunikation
+	 */
+	tmp_in = malloc(sizeof(int) * array_size);
+	tmp_out = malloc(sizeof(int) * array_size);
+	firstbuf = NULL;
 
+	/**
+	 * Wenn es nur einen Prozess gibt, matcht der erste automatisch den letzten.
+	 *  Daher kann man hier Zeit sparen. 
+	*/
+	if (world_size == 1)
+	{
+		return 0;
+	}
+
+	if (rank == world_size - 1)
+	{
+		firstbuf = malloc(sizeof(int) * array_size);
+	}
+
+
+	/**
+	 * Versenden vom letzten an den ersten Prozess für die Abbruchbedingung
+	*/
 	if (rank == 0)
 	{
-		printf("rank %d: Broadcast startet\n", rank);
-		MPI_Bcast(buf, array_size, MPI_INT, 0, MPI_COMM_WORLD);
-		printf("rank %d: Broadcast gesendet\n", rank);
+		MPI_Ssend(buf, array_size, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD);
 	}
-	else
+	else if (rank == world_size - 1)
 	{
-		MPI_Bcast(firstbuf, array_size, MPI_INT, 0, MPI_COMM_WORLD);
-		printf("rank %d: Broadcast empfangen\n", rank);
+		MPI_Recv(firstbuf, array_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 
-	printf("rank %d: Broadcast beendet\n", rank);
 
-	while(!b_iterate || 1)
+	while(!b_iterate)
 	{
-		printf("rank %d: startet circle", rank);
-		tmp = buf;
-		
+		iterations++;
 
-		if (rank == world_size - 1)
+		memcpy(tmp_out, buf, array_size * sizeof(int));
+
+		if (rank == 0)
 		{
-			printf("rank %d: sendet", rank);
-			MPI_Ssend(buf, array_size, MPI_INT, dest, 0, MPI_COMM_WORLD);
-			printf("rank %d: empfängt", rank);
-			MPI_Recv(buf, array_size, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Ssend(tmp_out, array_size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+			MPI_Recv(tmp_in, array_size, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		}
 		else
 		{
-			printf("rank %d: empfängt", rank);
-			MPI_Recv(buf, array_size, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			printf("rank %d: sendet", rank);
-			MPI_Ssend(tmp, array_size, MPI_INT, dest, 0, MPI_COMM_WORLD);
+			MPI_Recv(tmp_in, array_size, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Ssend(tmp_out, array_size, MPI_INT, dest, 0, MPI_COMM_WORLD);
 		}
 
+		memcpy (buf, tmp_in, array_size * sizeof(int));
+
 		/**
-		 * Check Termination
+		 * Überprüfe Abbruchbedingung. Broadcast aktualisiert die Abbruchbedingung für alle Prozesse.
 		*/
 		if (rank == world_size - 1)
 		{
 			b_iterate = (buf[0] == firstbuf[0])? 1 : 0;
+
 			MPI_Bcast(&b_iterate, 1, MPI_INT, world_size - 1, MPI_COMM_WORLD);
 		}
 		else
@@ -83,7 +105,16 @@ circle (int* buf, int array_size, int world_size, int rank)
 		}
 	}
 
-	printf("rank %d: beendet circle", rank);
+	free(tmp_in);
+	free(tmp_out);
+	if (rank == world_size - 1)
+	{
+		free(firstbuf);
+		printf("\n");
+		printf("Iterations: %d\n", iterations);
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
 	return 0;
 }
 
@@ -113,30 +144,22 @@ main (int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	if (N >= world_size)
-	{
-		proc_array_size = N / world_size;
-	}
-	else
-	{
-		proc_array_size = 1;
-	}
 	rest = N % world_size;
+	
+	proc_array_size = N / world_size;
+	proc_array_size += (rest != 0)? 1 : 0;
+
 	buf_ret = (rank == 0)? (int*)malloc(sizeof(int) * proc_array_size) : NULL;
-
-	// TODO: Prozesse müssen wissen, wie groß ihr Array ist, bzw. Reste verteilen
-
 	buf = init(proc_array_size, rank);
 
-	/*
+	/**
 	 *	Alle Prozesse haben die richtigen Elemente, die Prozesse mit "Lücken" kriegen illegale Werte,
 	 *	um die Kommunikation sauberer zu gestalten
 	 */
-	if (rank >= rest)
+	if (rest != 0 && rank >= rest)
 	{
 		buf[proc_array_size - 1] = 25;
 	}
-
 
 	if (rank == 0)
 	{
@@ -180,17 +203,23 @@ main (int argc, char** argv)
 		printf("rank 0: ");
 		for (int i = 0; i < proc_array_size; i++)
 		{
-			printf("%d ", buf[i]);
+			if (!(buf[i] == 25))
+			{
+				printf("%d ", buf[i]);
+			}
 		}
 		printf("\n");
 
 		for (int i = 1; i < world_size; i++)
 		{
-			MPI_Recv(buf, proc_array_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			printf("rank %d: ", rank);
+			MPI_Recv(buf_ret, proc_array_size, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			printf("rank %d: ", i);
 			for (int j = 0; j < proc_array_size; j++)
 			{
-				printf("%d", buf[j]);
+				if (!(buf_ret[j] == 25))
+				{
+					printf("%d ", buf_ret[j]);
+				}			
 			}
 			printf("\n");
 		}
@@ -202,5 +231,5 @@ main (int argc, char** argv)
 	free(buf);
 	MPI_Finalize();
 
-	return EXIT_SUCCESS;
+	return ret;
 }
