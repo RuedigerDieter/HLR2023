e87e0af09ca45368179d197860e3599fb7844497
Can <can.nayci@gmx.de>
2023-12-13 18:03:04 +0100

    fixed weird char

--- Blatt08/partdiff.c
+++ Blatt08/partdiff.c
@@ -645,7 +645,7 @@ main (int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	if(options.method == METH_GAUSS_SEIDEL || world_size == 1) {
+	if(options.method == METH_GAUSS_SEIDEL || world_size == 1) {
 		if(rank == 0) {
 
 			allocateMatrices(&arguments);
