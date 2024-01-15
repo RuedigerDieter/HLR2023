f6949c807661d1662a8dc530b175dfc5e4778322
Can <can.nayci@gmx.de>
2024-01-15 18:42:15 +0100

    added wrong lpp num for proc0 + N

--- Blatt10/partdiff.c
+++ Blatt10/partdiff.c
@@ -105,6 +105,9 @@ initVariablesMPI (struct calculation_arguments* arguments, struct calculation_re
 
 	lpp = lpp + (proc_args->rank < lpp_rest ? 1 : 0);
 	lpp += 2; // Platz für Halolines oben und unten
+	if(proc_args->rank == 0 || proc_args->rank == proc_args->world_size - 1)
+		lpp -= 1; // Platz für Halolines oben und unten
+	
 	proc_args->lpp = lpp;
 
 	uint64_t start_line = 0;
