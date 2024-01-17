88dcb26cb5c76dc1eec21d89188779be467105d9
RuedigerDieter <sharafemil@gmail.com>
2024-01-17 16:28:02 +0100

    rollback

--- Blatt10/partdiff.c
+++ Blatt10/partdiff.c
@@ -502,10 +502,6 @@ static void calculateMPI_GS (struct calculation_arguments const* arguments, stru
 	if(rank == 0 && options->termination == TERM_PREC){
 		//Beginne im Hintergrund das Empfangen der Nachricht von pN
 		MPI_Irecv(&N_to_0_PREC_REACHED, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD, &request);
-		if(N_to_0_PREC_REACHED == 1){
-			MPI_Wait(&request, MPI_STATUS_IGNORE);
-			//Abschluss :)
-		}
 	}
 
 	while (term_iteration > 0)
@@ -619,7 +615,7 @@ static void calculateMPI_GS (struct calculation_arguments const* arguments, stru
 			if (maxResiduum < options->term_precision && rank == world_size - 1)
 			{
 				N_to_0_PREC_REACHED = 1;
-				MPI_Ssend(&N_to_0_PREC_REACHED, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+				MPI_Issend(&N_to_0_PREC_REACHED, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
 				// printf("[%d] Sende PREC_REACHED an %d, %d\n", (int) rank, (int) 0, (int) term_iteration);
 			}
 			if (LAST_ITERATION)
