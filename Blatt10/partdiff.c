9eea58231a3ee3ac3b19f0d511c0e4ccbd909114
Emil Sharaf <sharafemil@gmail.com>
2024-01-17 12:37:19 +0100

    dbg info

--- Blatt10/partdiff.c
+++ Blatt10/partdiff.c
@@ -445,7 +445,7 @@ calculate (struct calculation_arguments const* arguments, struct calculation_res
  * calculateMPI_GS
  * Berechnet die Matrix mit dem Gauß-Seidel-Verfahren.
  * Known Issues/FIXME: Ab einer Interline-Zahl abhängig von Prozessanzahl kommt es zu Mismatch zwischen sequentieller und paralleler Berechnung.
- * 
+ * Auch Abhängig von Knoten-Prozess-Konfiguration: Richtiges Ergebnis bei 1x6, falsch bei 2x3.
 */
 static void calculateMPI_GS (struct calculation_arguments const* arguments, struct calculation_results* results, struct options const* options, struct process_arguments* proc_args)
 {
