e958c356ca317b886d50fc82022186920aee9f3d
Emil Sharaf <sharaf@wr.informatik.uni-hamburg.de>
2024-01-17 11:10:08 +0100

    funktioniert immer noch nicht

--- Blatt10/testnodeConfig.sh
+++ Blatt10/testnodeConfig.sh
@@ -7,8 +7,8 @@ for i in 1 2 3 4
 do
     for j in 1 2 3 4
     do
-        k = $((i*j))
+        k=$((i*j))
         echo "Nodes: $i, Tasks per Node: $j, Tasks: $k"
-        mpiexec -p west --nodes $i --ntasks-per-node $j --ntasks $k ./run.sh | grep "Norm"
+        srun -p west --nodes $i --ntasks-per-node $j --ntasks $k ./run.sh | grep "Norm"
     done
 done
