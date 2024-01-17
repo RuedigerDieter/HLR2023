da6643004d4a1e5b075d0bca80e218896bdb883f
Emil Sharaf <sharafemil@gmail.com>
2024-01-17 12:17:01 +0100

    nodes

--- Blatt10/testnodeConfig.sh
+++ Blatt10/testnodeConfig.sh
@@ -2,13 +2,14 @@
 #SBATCH -o output.out
 #SBATCH -p west
 #SBATCH --export=ALL
+#SBATCH -N 4
 
 for i in 1 2 3 4
 do
     for j in 1 2 3 4
     do
-        k = $((i*j))
+        k=$((i*j))
         echo "Nodes: $i, Tasks per Node: $j, Tasks: $k"
-        mpiexec -p west --nodes $i --ntasks-per-node $j --ntasks $k ./run.sh | grep "Norm"
+        srun -p west --nodes $i --ntasks-per-node $j --ntasks $k ./run.sh | grep "Norm"
     done
 done
