65dc730d350c3f5b4b571c95b1c328dcab3e23f5
Can <can.nayci@gmx.de>
2024-01-23 13:22:13 +0100

    moved files and added run.py

---
+++ Blatt11/run.py
@@ -0,0 +1,15 @@
+
+import os
+# assign directory
+directory = 'slurm'
+
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        if filename.endswith(".job"):
+            
+            #print("sbatch " + filename)
+            #os.system("sbatch slurm/" + filename)
