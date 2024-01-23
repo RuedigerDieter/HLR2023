c62e405081ea330335bf683a508b993d9f5251b3
yungcxn <can.nayci@gmx.de>
2024-01-23 13:17:13 +0100

    Added the files from aufg1

---
+++ Blatt11/run.py
@@ -0,0 +1,13 @@
+import os
+# assign directory
+directory = 'slurm'
+ 
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        if filename.endswith(".job"):
+            print("sbatch " + filename)
+            os.system("sbatch slurm/" + filename)  
